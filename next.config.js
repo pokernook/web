11c315fd807b44ccb174e8418aff91e1d38aa4cc
Ben Walker <bwalke05@gmail.com>
2021-04-01 18:51:35 -0400

    Add rewrite for local dev

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const API_GRAPHQL = process.env.API_GRAPHQL;
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
+
 module.exports = {
   future: {
     webpack5: true,
   },
   reactStrictMode: true,
+  rewrites: async () => {
+    return IS_PRODUCTION
+      ? []
+      : [{ source: "/api/graphql", destination: API_GRAPHQL }];
+  },
 };
