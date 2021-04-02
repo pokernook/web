eb2a7d1d9c37c8d9c4660ebb6fec7df435545f38
Ben Walker <bwalke05@gmail.com>
2021-04-01 23:56:44 -0400

    Use system color mode

--- src/chakra-theme/index.ts
+++ src/chakra-theme/index.ts
@@ -3,6 +3,9 @@ import { extendTheme, ThemeOverride } from "@chakra-ui/react";
 import { Button, Link } from "./components";
 
 const overrides: ThemeOverride = {
+  config: {
+    useSystemColorMode: true,
+  },
   components: {
     Button,
     Link,
