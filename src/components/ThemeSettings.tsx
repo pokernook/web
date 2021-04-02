317114aebd570b3001fef230da560b0bd307c0c6
Ben Walker <bwalke05@gmail.com>
2021-04-02 13:05:06 -0400

    Convert to Chakra

--- src/components/ThemeSettings.tsx
+++ src/components/ThemeSettings.tsx
@@ -1,15 +1,15 @@
+import { Box, Heading, Select, Text, useColorMode } from "@chakra-ui/react";
 import { ChangeEvent, FC } from "react";
-import { Box, Heading, Select, Text, useColorMode } from "theme-ui";
 
 export const ThemeSettings: FC = () => {
-  const [colorMode, setColorMode] = useColorMode();
+  const { colorMode, setColorMode } = useColorMode();
 
   const handleModeChange = (e: ChangeEvent<HTMLSelectElement>) =>
     setColorMode(e.target.value);
 
   return (
     <>
-      <Heading as="h3" mb={3}>
+      <Heading size="md" mb={3}>
         Theme
       </Heading>
 
