bcd9caf7a0950e933d9529d269ba809b90f89c20
Ben Walker <bwalke05@gmail.com>
2021-04-02 16:36:04 -0400

    Make wordwrap normal

--- src/theme/styles.ts
+++ src/theme/styles.ts
@@ -2,6 +2,9 @@ import { GlobalStyles, mode } from "@chakra-ui/theme-tools";
 
 export const styles: GlobalStyles = {
   global: (props) => ({
+    "*": {
+      wordWrap: "normal",
+    },
     body: {
       bg: mode("white", "black")(props) as string,
       color: mode("gray.800", "gray.200")(props) as string,
