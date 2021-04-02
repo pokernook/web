239826eae2fa11c546735ffdb43dd59456aa5bf5
Ben Walker <bwalke05@gmail.com>
2021-04-02 09:24:19 -0400

    Add global styles

--- src/chakra-theme/index.ts
+++ src/chakra-theme/index.ts
@@ -1,8 +1,10 @@
 import { extendTheme, ThemeOverride } from "@chakra-ui/react";
 
 import { Button, Link } from "./components";
+import { styles } from "./styles";
 
 const overrides: ThemeOverride = {
+  styles,
   config: {
     useSystemColorMode: true,
   },
